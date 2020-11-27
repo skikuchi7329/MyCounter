093cc386a1a3dfeee9a1bfff6bf03ced011b08c3
Shohei Kikuchi <killchance2256@gmail.com>
2020-11-27 21:29:43 +0900

    close 1st phase

--- js/main.js
+++ js/main.js
@@ -9,25 +9,76 @@ window.onload = function() {
   let count_up_btn3 = document.getElementById("Btn_countUp3");
   let count_up_btn4 = document.getElementById("Btn_countUp4");
   let count_up_btn5 = document.getElementById("Btn_countUp5");
-  let count_value = 0;
-
-  for(let i = 1; i <= 5; i++) {
-  count_up_btn.onclick = function() {
-    count_value += 1;
-    count_disp.innerHTML = count_value;
-  }
-  count_up_btn.onmousedown = function() {
-    count_up_btn.style.backgroundColor = "#00FF00";
-  }
-  count_up_btn.onmouseup = function() {
-    count_up_btn.style.backgroundColor = "";
-  }
-};
-
   let reset_btn = document.getElementById("Btn_reset");
+  let count_value1 = 0;
+  let count_value2 = 0;
+  let count_value3 = 0;
+  let count_value4 = 0;
+  let count_value5 = 0;
+
+  count_up_btn1.onclick = function() {
+    count_value1 += 1;
+    count_disp1.innerHTML = count_value1;
+  }
+  count_up_btn1.onmousedown = function() {
+    count_up_btn1.style.backgroundColor = "#00FF00";
+  }
+  count_up_btn1.onmouseup = function() {
+    count_up_btn1.style.backgroundColor = "";
+  }
+
+  count_up_btn2.onclick = function() {
+    count_value2 += 1;
+    count_disp2.innerHTML = count_value2;
+  }
+  count_up_btn2.onmousedown = function() {
+    count_up_btn2.style.backgroundColor = "#00FF00";
+  }
+  count_up_btn2.onmouseup = function() {
+    count_up_btn2.style.backgroundColor = "";
+  }
+
+  count_up_btn3.onclick = function() {
+    count_value3 += 1;
+    count_disp3.innerHTML = count_value3;
+  }
+  count_up_btn3.onmousedown = function() {
+    count_up_btn3.style.backgroundColor = "#00FF00";
+  }
+  count_up_btn3.onmouseup = function() {
+    count_up_btn3.style.backgroundColor = "";
+  }
+
+  count_up_btn4.onclick = function() {
+    count_value4 += 1;
+    count_disp4.innerHTML = count_value4;
+  }
+  count_up_btn4.onmousedown = function() {
+    count_up_btn4.style.backgroundColor = "#00FF00";
+  }
+  count_up_btn4.onmouseup = function() {
+    count_up_btn4.style.backgroundColor = "";
+  }
+
+  count_up_btn5.onclick = function() {
+    count_value5 += 1;
+    count_disp5.innerHTML = count_value5;
+  }
+  count_up_btn5.onmousedown = function() {
+    count_up_btn5.style.backgroundColor = "#00FF00";
+  }
+  count_up_btn5.onmouseup = function() {
+    count_up_btn5.style.backgroundColor = "";
+  }
+
+
 
   reset_btn.onclick = function() {
-    count_value = 0; count_disp.innerHTML = count_value;
+    count_value1 = 0; count_disp1.innerHTML = count_value1;
+    count_value2 = 0; count_disp2.innerHTML = count_value2;
+    count_value3 = 0; count_disp3.innerHTML = count_value3;
+    count_value4 = 0; count_disp4.innerHTML = count_value4;
+    count_value5 = 0; count_disp5.innerHTML = count_value5;
   }
   reset_btn.onmousedown = function() {
     reset_btn.style.backgroundColor = "#00FF00";
